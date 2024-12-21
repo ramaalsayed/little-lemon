f99c59c268ac71a1b32e70d1eeaacb6df3aa53af
ramaalsayed <rramma198@gmail.com>
2024-12-21 20:53:05 +0300

    first commit

---
+++ src/Header.js
@@ -0,0 +1,12 @@
+import logo from "./Logoo.png";
+export default function Header() {
+  return (
+    <header>
+      <img src={logo} alt="little lemon logo" />
+    </header>
+  );
+}
+// <meta name="description" content="Get all your sports fishing equipment at One Place Fishing. Open Monday to Friday, 9 to 5, in the Great Lake area."/>
+// <meta name="og:title" content="One Place Fishing"/>
+// <meta name="og:description" content="some description here"/>
+// <meta name="og:image" content="https://example.com/one-place-fishing-store.jpg"/>
---
+++ src/Nav.js
@@ -0,0 +1,36 @@
+export default function Nav() {
+  return (
+    <nav>
+      <ul>
+        <li>
+          <a href="/">Home</a>
+        </li>
+        <li>
+          <a href="/about">About</a>
+        </li>
+        <li>
+          <a href="/menu">Menu</a>
+        </li>
+        <li>
+          <a href="/reservations">Reservations</a>
+        </li>
+        <li>
+          <a href="/order-online">Order Online</a>
+        </li>
+        <li>
+          <a href="/login">Login</a>
+        </li>
+        {/* <li>
+          <a href="/home">Homepage</a>
+        </li>
+        <li>
+          <a href="/about">About Us</a>
+        </li>
+        <li>
+          <a href="/blog">Blog</a>
+        </li> */}
+      </ul>
+    </nav>
+  );
+}
+// <a href="/book" role="button">Book a table</a>
